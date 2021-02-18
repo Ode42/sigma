a4583ad307586de57dbd3ec77b9ea87c19348423
Ode42 <ode.progr@outlook.com>
2021-02-18 18:24:09 +0200

    Code documentation and small fixes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-const express = require("express");
+/* start of host variables */
 const port = process.env.PORT || 5000;
+const host = `http://localhost:${port}/`;
+const path = require("path");
+/* end of host variables */
+
+/* start of server variables */
+const express = require("express");
 const app = express();
-const routes = require("./routes");
 const mustacheExpress = require("mustache-express");
+ /* end of server variables*/
+
+ /* start of 1st party imports */
+const routes = require("./routes");
+ /* end of 1st party imports */
+
+ /* start of app configuration */
 app.engine("html", mustacheExpress());
 app.set("view engine", "html");
-app.set("views", __dirname + "/views");
-//router
+app.set("views", __dirname + "/public/views");
+app.use("/public", express.static(__dirname + "/public/"));
+app.use(express.static(__dirname + '/public'));
+/* end of app configuration */
 
-
+/* start of executing modules */
 routes(app);
+/* end of executing modules */
 
 // 404 error
 app.use((req, res) => {
-    res.status(404).sendFile("./views/404.html", {root: __dirname});
+    res.status(404).sendFile("./public/views/404.html", {root: __dirname});
 });
 
 // server listener
